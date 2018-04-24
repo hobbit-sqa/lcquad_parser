9af237471e08caf527405ee7cfc01958790cf6d5
aramgrigoryan <aramgrigoryan@protonmail.ch>
2018-04-24 23:00:27 +0200

    add logic

---
+++ src/constants.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+HOME_DIR = os.path.expanduser("~")
+DEFAULT_DATA_DIR = HOME_DIR
+
+DATA_FILE = os.path.join(DEFAULT_DATA_DIR, 'Lcquad_data.xlsx')
+OUTPUT_DATA_FILE = os.path.join(DEFAULT_DATA_DIR, 'questions.json')
+
+SPARQL = 'SPARQL'
+ORIGINAL = 'Original Question'
+MODIFIED = 'Paraphrased Question'
+COMMENTS = 'Comments/Remark'
+METRICS = 'Metrics'
---
+++ src/parse.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+from constants import DATA_FILE, OUTPUT_DATA_FILE, ORIGINAL, MODIFIED, METRICS
+
+import pandas as pd
+import json
+
+import nlgeval
+
+
+def get_data(file=DATA_FILE):
+    '''Reads the raw data from the file
+
+    :param file:
+    :return:
+    '''
+    # xls_file = pd.ExcelFile(file)
+    # print(xls_file.sheet_names)
+    # df = pd.DataFrame()
+    # for sheet_name in xls_file.sheet_names:
+    #    df.append(xls_file.parse(sheet_name))
+
+    # return df
+    # return pd.read_excel(file, sheet_name='sheet1')
+    df = pd.read_excel(file, sheet_name='sheet1')
+    df = df.append(pd.read_excel(file, sheet_name='sheet2'), ignore_index=True)
+    return df
+
+
+def save_json(data_dic, file=OUTPUT_DATA_FILE):
+    '''Saves dict as json file
+
+    :param data_dic:
+    :param file:
+    :return:
+    '''
+
+    with open(file, 'w') as f:
+        json.dumps(data_dic, f)
+
+
+def compute_metrics(hypothesis, references):
+    '''
+
+    :param hypothesis:
+    :param references:
+    :return:
+    '''
+
+    return nlgeval.compute_individual_metrics(hypothesis, references, False, True, False)
+
+
+def gen_data():
+    '''Generates data
+
+    :return:
+    '''
+    combined_dict = {}
+    data = get_data()
+    data = data[data[MODIFIED] != 'skip']
+    data = data[data[MODIFIED].notnull()]
+    for _, row in data.iterrows():
+        entry_dict = {}
+        entry_dict[ORIGINAL] = row[ORIGINAL]
+        entry_dict[MODIFIED] = row[MODIFIED]
+        entry_dict[METRICS] = compute_metrics(str(row[ORIGINAL]), str(row[MODIFIED]))
+        combined_dict[_] = entry_dict
+
+
+
+if __name__ == "__main__":
+    dt = gen_data()
